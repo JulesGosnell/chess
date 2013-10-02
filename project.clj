1ff684050bbf793fa116b34ea572323144d51b59
jules <jules_gosnell@yahoo.com>
2013-10-02 01:18:36 +0100

    time this was in version control

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject chess "0.1.0-SNAPSHOT"
+  :description "chess permutations"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.4"]]
+  :main chess.core
+  :jvm-opts ["-Xms4g" "-Xmx4g" "-server"]
+
+  )
